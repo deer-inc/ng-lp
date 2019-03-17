0b6e42f63a521c4a87c54488561ab57bffb58425
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-03-17 22:17:16 +0900

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgLpModule } from 'ng-lp';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgLpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
