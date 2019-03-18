ff5c9e9398c6845216e2b63888cc3588de1c21b1
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-03-18 10:39:57 +0900

    test: fix spec path

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NgLpModule } from 'projects/ng-lp/src/public_api';
+import { NgLpModule } from '@deer-inc/ng-lp';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 
